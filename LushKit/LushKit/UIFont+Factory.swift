40827107ad3cec96b413baefae5e0290109f12f0
acabico <acabico@gmail.com>
2016-12-13 17:50:27 +0000

    Remove storyboard, add handwritten font, update UIFont extension

--- LushKit/LushKit/UIFont+Factory.swift
+++ LushKit/LushKit/UIFont+Factory.swift
@@ -10,29 +10,96 @@ import Foundation
 import UIKit
 
 // standard font sizes
-public let kFontSmallestSize: CGFloat          = 11.0
-public let kFontSmallestSizeLowercase: CGFloat = 13.0
-public let kFontMediumSize: CGFloat            = 14.0
-public let kFontSmallSize: CGFloat             = 15.0
-public let kFontSmallNormalSize: CGFloat       = 16.0
-public let kFontNormalSize: CGFloat            = 18.0
-public let kFontTwentyTwoSize: CGFloat         = 22.0
-public let kFontLargeSize: CGFloat             = 30.0
-public let kFontLargerSize: CGFloat            = 47.0
-public let kFontLargestSize: CGFloat           = 56.0
+public let kFontHeading1Size: CGFloat       = 37.0
+public let kFontHeading2Size: CGFloat       = 23.0
+public let kFontHeading3Size: CGFloat       = 18.0
+public let kFontBodyCopySize: CGFloat       = 14.0
+public let kFontMiniBodyCopySize: CGFloat   = 11.0
 
 public extension UIFont {
     
-    public class func boldFontWithSize(size: CGFloat) -> UIFont {
+    public class func handwrittenHeading1Font() -> UIFont {
+        
+        let font = UIFont.handwrittenBoldFontWithSize(size: kFontHeading1Size)
+        
+        return font
+    }
+    
+    public class func heading1Font() -> UIFont {
+        
+        let font = UIFont.boldFontWithSize(size: kFontHeading1Size)
+        
+        return font
+    }
+    
+    public class func heading2Font() -> UIFont {
+        
+        let font = UIFont.boldFontWithSize(size: kFontHeading2Size)
+        
+        return font
+    }
+    
+    public class func handwrittenHeading2Font() -> UIFont {
+        
+        let font = UIFont.handwrittenBoldFontWithSize(size: kFontHeading2Size)
+        
+        return font
+    }
+    
+    public class func heading3Font() -> UIFont {
+        
+        let font = UIFont.boldFontWithSize(size: kFontHeading3Size)
+        
+        return font
+    }
+    
+    public class func handwrittenHeading3Font() -> UIFont {
+        
+        let font = UIFont.handwrittenBoldFontWithSize(size: kFontHeading3Size)
+        
+        return font
+    }
+    
+    public class func bodyCopyFont() -> UIFont {
+        
+        let font = UIFont.regularFontWithSize(size: kFontBodyCopySize)
+        
+        return font
+    }
+    
+    public class func miniBodyCopyFont() -> UIFont {
+        
+        let font = UIFont.regularFontWithSize(size: kFontMiniBodyCopySize)
+        
+        return font
+    }
+    
+    // MARK: - Helper methods
+    
+    class func boldFontWithSize(size: CGFloat) -> UIFont {
         
         let font: UIFont? = UIFont(name: "HelveticaNeue-Bold", size: size)
         
         return font!
     }
     
-    public class func regularFontWithSize(size: CGFloat) -> UIFont {
+    class func regularFontWithSize(size: CGFloat) -> UIFont {
         
         let font: UIFont? = UIFont(name: "HelveticaNeue", size: size)
+        
+        return font!
+    }
+    
+    class func handwrittenBoldFontWithSize(size: CGFloat) -> UIFont {
+        
+        let font: UIFont? = UIFont(name: "LushHandwritten-Bold", size: size)
+        
+        return font!
+    }
+    
+    class func handwrittenRegularFontWithSize(size: CGFloat) -> UIFont {
+        
+        let font: UIFont? = UIFont(name: "LushHandwritten-Regular", size: size)
         
         return font!
     }
